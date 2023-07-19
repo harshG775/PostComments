5383a5f24f1c45fe6a2be8a4772a7822947287f0
Harsh <hgaur491@gmail.com>
2023-07-19 16:14:57 +0530

    /

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,17 +7,15 @@ export default function App() {
         const fetchData = async () => { try {
                 const response = await fetch("http://localhost:4000/api/data");
                 const data = await response.json();
-				console.log(data)
-                // setServerData(data);
+				// console.log(data)
+                setServerData(data);
 		}catch (err) {console.log(err)}};
         fetchData();
     }, []);
     return (
 	<div>
-		{/* {serverData.users.map((user,i)=>(
-			<p key={i}>{user}</p>
-		))
-
-		}	 */}
+		{serverData.users?.map((user,i)=>(
+            <p key={i}>{user}</p>
+        ))}
 	</div>);
 }
