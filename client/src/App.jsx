e89d313faa5191064ffc67f839b9e4de9188d78b
Harsh <hgaur491@gmail.com>
2023-07-19 16:05:42 +0530

    C:/Program Files/Git/

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,22 @@ import { useEffect, useState } from "react";
 
 export default function App() {
     const [serverData, setServerData] = useState(["dummy data"]);
+	
     useEffect(() => {
         const fetchData = async () => { try {
                 const response = await fetch("http://localhost:4000/api/data");
                 const data = await response.json();
-                setServerData(data);
+				console.log(data)
+                // setServerData(data);
 		}catch (err) {console.log(err)}};
         fetchData();
     }, []);
     return (
 	<div>
-		{serverData.users.map((user,i)=>(
+		{/* {serverData.users.map((user,i)=>(
 			<p key={i}>{user}</p>
 		))
 
-		}	
+		}	 */}
 	</div>);
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+
 // app.use(express.urlencoded({extended:true}));
 // app.use(express.json())
-
+const dataBase = {
+    comments:[
+        {
+            username:"firstUser",
+            comment:"Lorem, ipsum dolor sit quae quasi ab, quos"
+        },
+        {
+            username:"firstUser",
+            comment:"Lorem, ipsum dolor sit quae quasi ab, quos"
+        }
+    ]
+}
 app.get('/api/data', (req, res) => {
     res.json({ users: ["userOne", "userTwo", "userThree"] });
 });
 
-const port = 4000
+const port = process.env.PORT || 4000
 app.listen(port,'0.0.0.0', () => { console.log(`server started on localhost:${port}`) });
