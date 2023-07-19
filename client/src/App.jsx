b7228a2ea6d76187c1ebf8c31ed470fb1f204744
Harsh <hgaur491@gmail.com>
2023-07-19 15:34:07 +0530

    connecting node server to vite/react client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [serverData, setServerData] = useState(["dummy data"]);
+    useEffect(() => {
+        const fetchData = async () => { try {
+                const response = await fetch("http://localhost:4000/api/data");
+                const data = await response.json();
+                setServerData(data);
+		}catch (err) {console.log(err)}};
+        fetchData();
+    }, []);
+    return (
+	<div>
+		{serverData.users.map((user,i)=>(
+			<p key={i}>{user}</p>
+		))
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+		}	
+	</div>);
 }
-
-export default App
--- server/server.js
+++ server/server.js
@@ -1,44 +1,11 @@
 const express = require('express');
-const path = require('path');
-const {v4:getId}=require("uuid")
 const app = express();
+// app.use(express.urlencoded({extended:true}));
+// app.use(express.json())
 
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.set("views", path.join(__dirname,"public"))
-app.set("view engine","ejs")
-
-app.use(express.urlencoded({extended:true}));
-app.use(express.json())
-
-const comments= [
-    {   
-        id:getId(),
-        username:"harsh",
-        comment:"Setting overflow to scroll or auto will display scrollbars to reveal the additional text, while hidden will not. The hidden text can be selected by selecting the ellipses."
-    }
-]
-
-app.get("/post", (req, res) => {
-    res.render('post/index.ejs',{post:"this is the post",comments});
+app.get('/api/data', (req, res) => {
+    res.json({ users: ["userOne", "userTwo", "userThree"] });
 });
 
-
-/*create part of CRUD*/
-app.post("/post/comment/new",(req,res)=>{
-    const{username,comment}=(req.body)
-    comments.push({username,comment})
-    res.redirect("/post")
-})
-
-/*updating part of CRUD*/
-app.post("/post/comment/edit/:id",(req,res)=>{
-    console.log(req.params)
-})
-
-
-
-const port =3000
-app.listen(port, () => {
-    console.log(`server started at port  http://localhost:${port}/post`);
-});
+const port = 4000
+app.listen(port,'0.0.0.0', () => { console.log(`server started on localhost:${port}`) });
